40df8199b8f4f8664c6e7d1e93086c636a623647
Bryan Rodrigues <slaters@gmail.com>
2016-05-03 14:12:18 -0400

    Added React Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form'
 
-class PostsNew extends Component{
-    render(){
-        return(
+class PostsNew extends Component {
+    render() {
+        return (
             <div>Create Form</div>
+
         );
     }
 }
 
-export default PostsNew;
+export default reduxFrom({
+    form: 'PostNew',
+    fields: ['title', 'categories', 'content']
+})(PostsNew);
