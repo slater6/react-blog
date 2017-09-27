6dcb629ee37595485b62e50c86a49cf01ba711ff
slater6 <slaters@gmail.com>
2017-09-26 22:10:23 -0400

    Finished Redux Course

--- js-framerwork-voting-app/src/App.js
+++ js-framerwork-voting-app/src/App.js
@@ -1,18 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { voteAngular, voteReact, voteVue } from './actions'
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    
+    super(props);
+
+    this.store = this.props.store;
+  }
+
+  handleVoteAngular = () => {
+    this.store.dispatch(voteAngular());
+  }
+
+  handleVoteReact = () => {
+    this.store.dispatch(voteReact());
+  }
+
+  handleVoteVue = () => {
+    this.store.dispatch(voteVue());
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div>
+        <div className="jumbotron" style={{'textAlign': 'center'}}>
+          <h2>What is your favorite front-end development framework 2017?</h2>
+          <h4>Click on the logos below to vote!</h4>
+          <br />
+          <div className="row">
+            <div className="col-xs-offset-3 col-xs-2">
+              <img src="/img/angular.png" height="96" alt="Angular" onClick={this.handleVoteAngular}></img>
+            </div>
+            <div className="col-xs-2">
+              <img src="/img/react.png" height="96" alt="React" onClick={this.handleVoteReact}></img>
+            </div>
+            <div className="col-xs-2">
+              <img src="/img/vue.png" height="96" alt="Vue.js" onClick={this.handleVoteVue}></img>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- js-framerwork-voting-app/src/index.js
+++ js-framerwork-voting-app/src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore } from 'redux'
+import myApp from './reducers'
 import './index.css';
 import App from './App';
+import Results from './components/results';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(myApp,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+function render(){
+    ReactDOM.render(
+        <div className="container">
+            <App store={store}/>
+            <Results store={store}/>
+        </div>, 
+    document.getElementById('root'));
+}
+
+store.subscribe(render)
+
+render();
+
 registerServiceWorker();
