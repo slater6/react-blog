dcf06a38ac5e3485a339da60dd0641a5bdedac96
Bryan Rodrigues <brodrigues@weareinteractive.ca>
2018-04-04 13:19:38 -0400

    Added More Tests with state

--- react-testing/src/components/App.test.js
+++ react-testing/src/components/App.test.js
@@ -7,3 +7,18 @@ const app = shallow(<App />);
 it('render correctly', () => {
   expect(app).toMatchSnapshot();
 });
+
+it('initializes the `state` with and empty list of gifts', () => {
+  expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift `state` when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+  expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
+
+it('adds a new gift to the rendered list when clicking the `add gift` button', () => {
+  app.find('.btn-add').simulate('click');
+  const giftListLength = app.find('.gift-list').children().length;
+  expect(giftListLength).toEqual(2);
+});
