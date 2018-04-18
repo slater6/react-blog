0668b75b060d448b4601b5ba7a6f11bcf67ec229
Bryan Rodrigues <brodrigues@weareinteractive.ca>
2018-04-18 12:50:05 -0400

    Setup Tests for Reading Cookies

--- react-testing/loot-check/src/reducers/balance.test.js
+++ react-testing/loot-check/src/reducers/balance.test.js
@@ -1,13 +1,22 @@
 import balanceReducer from './balance';
+import balanceReducer2 from './balance';
 import * as constants from '../actions/constants';
 
 describe('balanceReducer', () => {
-  it('sets a balance', () => {
+  describe('when initializing', () => {
     const balance = 10;
 
-    expect(
-      balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
-    ).toEqual(balance);
+    it('sets a balance', () => {
+      expect(
+        balanceReducer(undefined, { type: constants.SET_BALANCE, balance })
+      ).toEqual(balance);
+    });
+
+    describe('then re-intializing', () => {
+      it('reads the balance from cookies', () => {
+        expect(balanceReducer2(undefined, {})).toEqual(balance);
+      });
+    });
   });
 
   it('deposits into the balance', () => {
